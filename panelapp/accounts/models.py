5eba61590cfca31a10c8635cb9c953414ac0a764
Oleg Gerasimenko <oleg.gerasimenko@nhs.net>
2017-05-18 15:47:46 +0100

    initial commit, base app is setup

---
+++ panelapp/accounts/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
+
+from model_utils.models import TimeStampedModel
+
+
+class User(AbstractUser, TimeStampedModel):
+    pass
+
+
+class Reviewers(models.Model):
+    user = models.OneToOneField(User)
+    user_type = models.CharField(max_length=255)
+    affiliation = models.CharField(max_length=255)
+    workplace = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+    group = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.user)
---
+++ panelapp/panelapp/settings/dev.py
@@ -0,0 +1,4 @@
+from .base import *  # noqa
+
+
+RUNSERVERPLUS_SERVER_ADDRESS_PORT = '0.0.0.0:8000'
