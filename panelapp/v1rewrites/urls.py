1d0257d11f4664c26d70f56efcdc4826464679cd
Oleg Gerasimenko <oleg.gerasimenko@nhs.net>
2017-10-27 16:33:37 +0100

    Fixed evaluations url rewrite fix (v1 -> v2), and added url rewrite tests

--- panelapp/v1rewrites/urls.py
+++ panelapp/v1rewrites/urls.py
@@ -8,13 +8,13 @@ from .views import RedirectWebServices
 from .views import RedirectGenePanelView
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url="/", permanent=False)),
-    url(r'^PanelApp/$', RedirectView.as_view(url="/", permanent=False)),
-    url(r'^PanelApp/Genes$', RedirectView.as_view(url="/panels/genes/", permanent=False)),
-    url(r'^PanelApp/Genes/(?P<gene_symbol>.*)$', RedirectGeneView.as_view()),
-    url(r'^PanelApp/PanelBrowser$', RedirectView.as_view(url=reverse_lazy("panels:index"))),
-    url(r'^PanelApp/EditPanel/(?P<old_pk>[a-z0-9]+)$', RedirectPanelView.as_view()),
+    url(r'^$', RedirectView.as_view(url="/", permanent=True)),
+    url(r'^PanelApp/$', RedirectView.as_view(url="/", permanent=True)),
+    url(r'^PanelApp/Genes$', RedirectView.as_view(url="/panels/genes/", permanent=True)),
+    url(r'^PanelApp/Genes/(?P<gene_symbol>.*)$', RedirectGeneView.as_view(permanent=True)),
+    url(r'^PanelApp/PanelBrowser$', RedirectView.as_view(url=reverse_lazy("panels:index"), permanent=True)),
+    url(r'^PanelApp/EditPanel/(?P<old_pk>[a-z0-9]+)$', RedirectPanelView.as_view(permanent=True)),
     url(r'^PanelApp/GeneReview/(?P<old_pk>[a-z0-9]+)/(?P<gene_symbol>.*)$',
-        RedirectGenePanelView.as_view()),
-    url(r'^WebServices/(?P<ws>.*)$', RedirectWebServices.as_view()),
+        RedirectGenePanelView.as_view(permanent=True)),
+    url(r'^WebServices/(?P<ws>.*)$', RedirectWebServices.as_view(permanent=True)),
 ]
